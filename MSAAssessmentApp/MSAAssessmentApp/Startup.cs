15655c6bc5fc0bf07714dff02dcaaeb1d9b2117e
Niclas von Ahsen <seekerofpie@hotmail.com>
2015-11-27 00:35:30 +1300

    First Commit Second Half

---
+++ MSAAssessmentApp/MSAAssessmentApp/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MSAAssessmentApp.Startup))]
+
+namespace MSAAssessmentApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            app.MapSignalR();
+        }
+    }
+}
