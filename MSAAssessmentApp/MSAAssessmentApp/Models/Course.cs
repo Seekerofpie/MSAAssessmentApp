878a49dd4d0c9e2d85e90d4ce7766e1a717fa68f
Niclas von Ahsen <seekerofpie@hotmail.com>
2015-11-27 00:24:06 +1300

    First half of first commit

---
+++ MSAAssessmentApp/MSAAssessmentApp/Models/Course.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using Newtonsoft.Json;
+
+namespace MSAAssessmentApp.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Points { get; set; }
+
+        [JsonIgnore]
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+        [JsonIgnore]
+        public virtual ICollection<Assignment> Assignments { get; set; }
+    }
+}
