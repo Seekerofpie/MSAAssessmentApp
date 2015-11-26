008da79450d882efc9d9ab7c7a0c83b3699434b2
Niclas von Ahsen <seekerofpie@hotmail.com>
2015-11-26 23:58:13 +1300

    First Commit

---
+++ MSAAssessmentApp/MSAAssessmentApp/Models/Assignment.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MSAAssessmentApp.Models
+{
+    public enum Status
+    {
+        Ongoing, Upcoming, Completed
+    }
+
+    public class Assignment
+    {
+        public int AssignmentID { get; set; }
+        public int CourseID { get; set; }
+        public string Name { get; set; }
+        public DateTime DueDate { get; set; }
+        public int? Percentage { get; set; }
+        public int? MyWeightedMark { get; set; }
+        public Status CurrentStatus { get; set; }
+
+        [JsonIgnore]
+        public virtual Course course{ get; set; }
+
+    }
+}
